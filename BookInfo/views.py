e1b13b498aca819e40e6941990a0eacfdec1a24e
KumaKuma0810 <windn747@gmail.com>
2024-12-18 23:06:39 +0300

    Add .gitignore

--- BookInfo/views.py
+++ BookInfo/views.py
@@ -1,53 +1,54 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+# from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# from django.urls import reverse_lazy
 
 from .models import Book, Review, ReadListBook, User
 
-class BookList(request): 
+def BookList(request): 
     books = Book.objects.all() 
-    return render(request, 'book-list.html', context={"books": books} )
+
+    return render(request, 'BookInfo/book_list.html')
 
     # model = Book
     # template_name = 'BookInfo/book-list.html'
     # content_object_name = 'books'
 
 
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'BookInfo/book_detail.html'
-    content_object_name = 'book'
+# class BookDetailView(DetailView):
+#     model = Book
+#     template_name = 'BookInfo/book_detail.html'
+#     content_object_name = 'book'
 
 
-class BookCreateView(CreateView):
-    model = Book
-    template_name = 'BookInfo/book_add.html'
-    fields = [
-        'username', 
-        'avtor', 
-        'genre', 
-        'year_of_publication', 
-        'description', 
-        'cover', 
-        'date_added']
-    book_url = reverse_lazy('book-add')
+# class BookCreateView(CreateView):
+#     model = Book
+#     template_name = 'BookInfo/book_add.html'
+#     fields = [
+#         'username', 
+#         'avtor', 
+#         'genre', 
+#         'year_of_publication', 
+#         'description', 
+#         'cover', 
+#         'date_added']
+#     book_url = reverse_lazy('book-add')
     
 
-class BookUpdateView(UpdateView):
-    model = Book
-    tempalate_name = 'BookInfo/book_form.html'
-    fields = [
-        'username', 
-        'avtor', 
-        'genre', 
-        'year_of_publication', 
-        'description', 
-        'cover', 
-        'date_added']
-    success_url = reverse_lazy('book-edit')
+# class BookUpdateView(UpdateView):
+#     model = Book
+#     tempalate_name = 'BookInfo/book_form.html'
+#     fields = [
+#         'username', 
+#         'avtor', 
+#         'genre', 
+#         'year_of_publication', 
+#         'description', 
+#         'cover', 
+#         'date_added']
+#     success_url = reverse_lazy('book-edit')
 
 
-class BookDeleteView(DeleteView):
-    model = Book
-    template_name = 'book_confirm_delete.html'
-    success_url = reverse_lazy('book-delete')
+# class BookDeleteView(DeleteView):
+#     model = Book
+#     template_name = 'book_confirm_delete.html'
+#     success_url = reverse_lazy('book-delete')
